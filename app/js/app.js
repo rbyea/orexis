f3045379998b0b2a688672669c2ddaa460a4e1ab
rbyea <csgo.roby@gmail.com>
2026-07-24 16:10:31 +0300

    24.07

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,49 @@
 import { device_width, gsap_ease } from "@utils/constants.js"
 
-document.addEventListener("DOMContentLoaded", () => {})
+document.addEventListener("DOMContentLoaded", () => {
+	if (window.innerWidth < 1024) {
+		new Swiper(".advantages__list-swiper", {
+			slidesPerView: 3,
+			spaceBetween: 8,
+			slidesPerGroup: 3,
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					slidesPerGroup: 1,
+				},
+				640: {
+					slidesPerView: 1,
+					slidesPerGroup: 1,
+				},
+				768: {
+					slidesPerView: 2,
+					slidesPerGroup: 2,
+				},
+				1024: {
+					slidesPerView: 3,
+					slidesPerGroup: 3,
+				},
+			},
+		})
+	}
+
+	new Swiper(".community-swiper", {
+		slidesPerView: 2,
+		spaceBetween: 20,
+		slidesPerGroup: 1,
+		navigation: {
+			nextEl: ".gallery__control--next",
+			prevEl: ".gallery__control--prev",
+		},
+	})
+
+	new Swiper(".reviews-swiper", {
+		slidesPerView: 2,
+		spaceBetween: 20,
+		slidesPerGroup: 1,
+		navigation: {
+			nextEl: ".gallery__control--next",
+			prevEl: ".gallery__control--prev",
+		},
+	})
+})
