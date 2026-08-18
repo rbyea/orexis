0e2216d7f4dd809f2ca6fe3345551d20d24664cd
rbyea <csgo.roby@gmail.com>
2026-08-18 16:35:57 +0300

    18.08

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,18 @@
 import { device_width, gsap_ease } from "@utils/constants.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-	$(".program-block").matchHeight({
-		byRow: true,
-	})
+	const syncProgramBlocksHeight = () => {
+		$(".program-block").matchHeight({
+			byRow: true,
+			remove: true,
+		})
+
+		$(".program-block").matchHeight({
+			byRow: true,
+		})
+	}
+
+	syncProgramBlocksHeight()
 	$(".advantages__item").matchHeight({
 		byRow: true,
 	})
@@ -37,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		$(".programs__list .swiper-slide").wrapAll(
 			'<div class="swiper-wrapper"></div>',
 		)
-		new Swiper(".programs-swiper", {
+		const programsSwiper = new Swiper(".programs-swiper", {
 			slidesPerView: 3,
 			spaceBetween: 8,
 			slidesPerGroup: 1,
@@ -64,6 +73,9 @@ document.addEventListener("DOMContentLoaded", () => {
 				},
 			},
 		})
+
+		programsSwiper.on("init resize slideChange transitionEnd", syncProgramBlocksHeight)
+		syncProgramBlocksHeight()
 	}
 
 	if (window.innerWidth < 1024) {
@@ -95,26 +107,58 @@ document.addEventListener("DOMContentLoaded", () => {
 		})
 	}
 
+	const isTouchCommunity = window.matchMedia("(max-width: 992px)").matches
+
 	new Swiper(".community-swiper", {
-		slidesPerView: 2,
-		spaceBetween: 20,
+		slidesPerView: 3,
+		spaceBetween: 12,
 		slidesPerGroup: 1,
+		cssMode: isTouchCommunity,
+		preventClicks: false,
+		preventClicksPropagation: false,
+		touchStartPreventDefault: false,
 		navigation: {
 			nextEl: ".gallery__control--next",
 			prevEl: ".gallery__control--prev",
 		},
+		pagination: {
+			el: ".community-pagination",
+			clickable: true,
+		},
 		breakpoints: {
 			320: {
-				slidesPerView: 1.2,
-			},
-			468: {
 				slidesPerView: 2,
+				spaceBetween: 8,
+			},
+			640: {
+				slidesPerView: 2,
+				spaceBetween: 8,
 			},
 			1024: {
-				slidesPerView: 2,
+				slidesPerView: 3,
+				spaceBetween: 12,
 			},
 		},
 	})
+
+	document
+		.querySelectorAll(".community-slide__video iframe")
+		.forEach((iframe) => {
+			const video = iframe.closest(".community-slide__video")
+
+			if (!video) {
+				return
+			}
+
+			const hideLoader = () => {
+				video.classList.add("is-loaded")
+			}
+
+			iframe.addEventListener("load", hideLoader)
+			window.setTimeout(hideLoader, 4000)
+		})
+
+	window.addEventListener("load", syncProgramBlocksHeight)
 
 	new Swiper(".reviews-swiper", {
 		slidesPerView: 2,
